804340cc0f0074c7dd967cc4370453e32181547f
Abhinav <you@example.comabhinavprasanth06@gmail.com>
2026-01-16 15:57:19 +0530

    added count

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
 public class HelloWorld {
-    // TODO: Write your program here to print the required output
+    public static void main(String[] args){
+		Scanner sc = new  Scanner(System.in);
+		System.out.print("enter the word");
+		String word = sc.next();
+		System.out.print("enter the letter");
+		char letter = sc.next().charAt(0);
+
+		int count = 0;
+		int len = word.length();
+		for(int i =0;i<len;i++){
+			if(word.charAt(i) == letter){
+				count++;
+			}
+		}
+		System.out.print(count);
+	}
+
 }
